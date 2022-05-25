28da53f794a8072ca46a422077eed39386158b52
emily-nicole-aguiar <106185441+emily-nicole-aguiar@users.noreply.github.com>
2022-05-25 16:21:49 -0400

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -5,6 +5,7 @@ public class Professor {
 
     void exibir(){
         
+        //Inserir dados do professor
         System.out.println("Professor: "+nome);
         System.out.println("Disciplina ministrada: "+disciplina);
         System.out.println("Nota: "+nota);
